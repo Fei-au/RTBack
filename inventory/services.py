2a7ae858b257e983c15f84df7398b6077beea808
fei <fei-ca@outlook.com>
2024-03-07 15:53:06 -0500

    update

--- inventory/services.py
+++ inventory/services.py
@@ -103,6 +103,7 @@ def get_image_urls(url):
             print('click each next image')
 
             # Use explicit wait to wait for the <li> elements with class prefix "image item item" to be present
+
             wait = WebDriverWait(driver, 10)
             li_elements = wait.until(
                 EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'li[class^="image item item"]')))
@@ -121,6 +122,8 @@ def get_image_urls(url):
                 print(f"Image {idx}: {iURL}")
 
             return image_urls, raw_html, current_url
+        except TimeoutException:
+            print("The elements did not load within the timeout period.")
         except Exception as e:
             print('download img error', e)
         finally:
